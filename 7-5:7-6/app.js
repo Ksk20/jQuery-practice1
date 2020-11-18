e9be2e0c454f858bcee149fae1686a8b5326a326
ksk <ksk@ksknoMacBook-Pro.local>
2020-11-18 15:56:53 +0900

    jQuery-practice

---
+++ 7-5:7-6/app.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.box1').on('click', function(){
+		$('.box1').addClass('box1-ext');
+	});
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
+	});
+
+	$("#button").on("click",function(){
+		$("#target").fadeOut("slow");
+	});
+});
